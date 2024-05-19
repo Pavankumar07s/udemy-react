6f86f7583261581184ee382cdd179215812bc254
Pavankumar07s <pawankumar14662693@gmail.com>
2024-05-19 14:42:03 +0530

    example button added using menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { CORE_CONCEPTS } from "./data";
 
 import Header from "./components/Header"
 import CoreConcept from "./components/Coreconcept"
-
+import TabButton from "./components/TabButton";
 function App() {
   return (
+    <main>
   <div>
     <Header></Header>
     <section id="core-concepts">
@@ -16,10 +17,19 @@ function App() {
         <CoreConcept {...CORE_CONCEPTS[3]}/>
       </ul>
     </section>
-    <main>
-      <h2>Time to get started!</h2>
-    </main>
-  </div>
+
+      <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+          <TabButton>Componets</TabButton>
+          <TabButton>Jsx</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>States</TabButton>
+        </menu>
+      </section>
+        
+    </div>
+  </main>
   );
     
   
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton(props){
+    return(
+        <li>
+            <button>{props.children}</button>
+        </li>
+    )
+}
+
