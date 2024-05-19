82c41042966de71964abf335fa668f0b1b7e087a
Pavankumar07s <pawankumar14662693@gmail.com>
2024-05-19 09:23:35 +0530

    dynammic done :}

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import Reactimage from "./assets/state-mgmt.png"
+
+const arrayForDiscription=["hiii","hello","holla"]
+const gentRandomIndex=(max)=>{
+  return Math.floor(Math.random()*(max+1))
+}
+
+function Header(){
+  let discription =arrayForDiscription[gentRandomIndex(2)]
+  return(
+    <header>
+      <img src={Reactimage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {discription} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+    
+  )
+}
+
 function App() {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
+  <div>
+    <Header></Header>
+    <main>
+      <h2>Time to get started!</h2>
+    </main>
+  </div>
   );
+    
+  
 }
 
 export default App;
