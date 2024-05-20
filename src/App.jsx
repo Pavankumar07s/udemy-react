6171f0097fa772978c093e8f01ce97417b4dd7d1
Pavankumar07s <pawankumar14662693@gmail.com>
2024-05-20 12:50:01 +0530

    dynamic data fetching in core concept

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     <Header></Header>
     <section id="core-concepts">
       <ul>
-        {CORE_CONCEPTS.map((conceptItem)=>(<CoreConcept {...conceptItem}/>))}
+        {CORE_CONCEPTS.map((conceptItem)=>(<CoreConcept key={conceptItem.title}{...conceptItem}/>))}
         {/* <CoreConcept {...CORE_CONCEPTS[0]}/>
         <CoreConcept {...CORE_CONCEPTS[1]}/>
         <CoreConcept {...CORE_CONCEPTS[2]}/>
